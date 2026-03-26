0a8053b95905b538e33c2008ca0deea9b9db417f
AaravBaori <e1459317@u.nus.edu>
2026-03-26 20:49:29 +0800

    Fix Style 2

--- src/main/java/ccamanager/command/AddCcaCommand.java
+++ src/main/java/ccamanager/command/AddCcaCommand.java
@@ -3,7 +3,6 @@ package ccamanager.command;
 import ccamanager.enumerations.CcaLevel;
 import ccamanager.manager.CcaManager;
 import ccamanager.manager.ResidentManager;
-import ccamanager.model.Cca;
 import ccamanager.ui.Ui;
 
 import ccamanager.exceptions.DuplicateCcaException;
