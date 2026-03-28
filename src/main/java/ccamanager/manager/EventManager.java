6d7a357d9e838507f649b8e2fab9014e649b68ba
Veer Mehta <e1357046@u.nus.edu>
2026-03-28 19:17:10 +0800

    Edit addEvent method in the eventManager to handle duplicate events

--- src/main/java/ccamanager/manager/EventManager.java
+++ src/main/java/ccamanager/manager/EventManager.java
@@ -19,17 +19,28 @@ public class EventManager {
         events = new ArrayList<>();
     }
 
-
+    /**
+     * Add a new event and store it in the events
+     * @param eventName Name of the event
+     * @param cca The CCA event is added in
+     * @param eventDate Date or time of the event
+     * @throws DuplicateEventException Handles the duplicate events
+     */
     public void addEvent(String eventName, Cca cca, String eventDate) throws DuplicateEventException {
         boolean isDuplicate = events.stream()
-                .anyMatch(x -> x.getEventName().equalsIgnoreCase(eventName));
+                .anyMatch(x -> x.getEventName().equalsIgnoreCase(eventName)
+                        && x.getCca().equals(cca)
+                        && x.getEventDate().equals(eventDate));
+
         if (isDuplicate) {
-            logger.log(Level.WARNING,"Resident with the same matric number " + eventName + " already exists.");
-            throw new DuplicateEventException("Resident with matric number " + eventName + " already exists.");
+            logger.log(Level.WARNING, "Event already exists: " + eventName + " for " + cca.getName() + " on " + eventDate);
+            throw new DuplicateEventException("Event '" + eventName + "' already exists for this CCA on this date.");
         }
+
         events.add(new Event(eventName, cca, eventDate));
-        logger.log(Level.INFO, "Successfully added resident: {0}", eventName);
+        logger.log(Level.INFO, "Successfully added event: {0}", eventName);
     }
+
     
     public ArrayList<Event> getEventList() {
         return events;
