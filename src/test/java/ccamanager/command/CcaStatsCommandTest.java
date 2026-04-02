5fdb76dc216a77ac56144c80feb253d957da57d5
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-04-02 20:45:36 +0800

    style: remove unused imports

--- src/test/java/ccamanager/command/CcaStatsCommandTest.java
+++ src/test/java/ccamanager/command/CcaStatsCommandTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CcaStatsCommandTest {
