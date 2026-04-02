2abdeb2e398e888efcfdb46a2c6890ba60f4970e
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-04-02 21:06:34 +0800

    test: update input commands to include ; separator

--- src/test/java/ccamanager/command/CcaStatsCommandTest.java
+++ src/test/java/ccamanager/command/CcaStatsCommandTest.java
@@ -31,17 +31,17 @@ public class CcaStatsCommandTest {
 
     @Test
     void avgPoints_success() {
-        Command addCcaBasketball = parser.parse("add-cca Basketball HIGH");
+        Command addCcaBasketball = parser.parse("add-cca Basketball; HIGH");
         addCcaBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaFootball = parser.parse("add-cca Football MEDIUM");
+        Command addCcaFootball = parser.parse("add-cca Football; MEDIUM");
         addCcaFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJohn = parser.parse("add-resident John 1234");
+        Command addResidentJohn = parser.parse("add-resident John; 1234");
         addResidentJohn.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJames = parser.parse("add-resident James 4321");
+        Command addResidentJames = parser.parse("add-resident James; 4321");
         addResidentJames.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234 Basketball 9");
+        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234; Basketball; 9");
         addJohnToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321 Basketball 8");
+        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321; Basketball; 8");
         addJamesToBasketball.execute(ccaManager, residentManager, eventManager, ui);
         ArrayList<Cca> ccas = ccaManager.getCCAList();
         assert ccas.size() == 2 : "There should be 2 CCAs.";
@@ -66,23 +66,23 @@ public class CcaStatsCommandTest {
 
     @Test
     void mostPopularCcas_success() {
-        Command addCcaBasketball = parser.parse("add-cca Basketball HIGH");
+        Command addCcaBasketball = parser.parse("add-cca Basketball; HIGH");
         addCcaBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaFootball = parser.parse("add-cca Football MEDIUM");
+        Command addCcaFootball = parser.parse("add-cca Football; MEDIUM");
         addCcaFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaTennis = parser.parse("add-cca Tennis LOW");
+        Command addCcaTennis = parser.parse("add-cca Tennis; LOW");
         addCcaTennis.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJohn = parser.parse("add-resident John 1234");
+        Command addResidentJohn = parser.parse("add-resident John; 1234");
         addResidentJohn.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJames = parser.parse("add-resident James 4321");
+        Command addResidentJames = parser.parse("add-resident James; 4321");
         addResidentJames.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJane = parser.parse("add-resident Jane 5678");
+        Command addResidentJane = parser.parse("add-resident Jane; 5678");
         addResidentJane.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234 Basketball 9");
+        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234; Basketball; 9");
         addJohnToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321 Football 9");
+        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321; Football; 9");
         addJamesToFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678 Tennis 8");
+        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678; Tennis; 8");
         addJaneToTennis.execute(ccaManager, residentManager, eventManager, ui);
         ArrayList<Cca> ccas = ccaManager.getCCAList();
         assert ccas.size() == 3 : "There should be 3 CCAs.";
@@ -100,21 +100,21 @@ public class CcaStatsCommandTest {
 
     @Test
     void mostActiveResidents_success() {
-        Command addCcaBasketball = parser.parse("add-cca Basketball HIGH");
+        Command addCcaBasketball = parser.parse("add-cca Basketball; HIGH");
         addCcaBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaTennis = parser.parse("add-cca Tennis LOW");
+        Command addCcaTennis = parser.parse("add-cca Tennis; LOW");
         addCcaTennis.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJohn = parser.parse("add-resident John 1234");
+        Command addResidentJohn = parser.parse("add-resident John; 1234");
         addResidentJohn.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJames = parser.parse("add-resident James 4321");
+        Command addResidentJames = parser.parse("add-resident James; 4321");
         addResidentJames.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJane = parser.parse("add-resident Jane 5678");
+        Command addResidentJane = parser.parse("add-resident Jane; 5678");
         addResidentJane.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234 Basketball 9");
+        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234; Basketball; 9");
         addJohnToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321 Basketball 8");
+        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321; Basketball; 8");
         addJamesToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678 Tennis 8");
+        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678; Tennis; 8");
         addJaneToTennis.execute(ccaManager, residentManager, eventManager, ui);
         ArrayList<Cca> ccas = ccaManager.getCCAList();
         assert ccas.size() == 2 :  "There should be 2 CCAs.";
--- src/test/java/ccamanager/command/ResidentStatsCommandTest.java
+++ src/test/java/ccamanager/command/ResidentStatsCommandTest.java
@@ -30,19 +30,19 @@ public class ResidentStatsCommandTest {
 
     @Test
     void totalPoints_success() {
-        Command addCcaBasketball = parser.parse("add-cca Basketball HIGH");
+        Command addCcaBasketball = parser.parse("add-cca Basketball; HIGH");
         addCcaBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaFootball = parser.parse("add-cca Football MEDIUM");
+        Command addCcaFootball = parser.parse("add-cca Football; MEDIUM");
         addCcaFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJohn = parser.parse("add-resident John 1234");
+        Command addResidentJohn = parser.parse("add-resident John; 1234");
         addResidentJohn.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJames = parser.parse("add-resident James 4321");
+        Command addResidentJames = parser.parse("add-resident James; 4321");
         addResidentJames.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234 Basketball 9");
+        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234; Basketball; 9");
         addJohnToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321 Basketball 8");
+        Command addJamesToBasketball = parser.parse("add-resident-to-cca 4321; Basketball; 8");
         addJamesToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321 Football 9");
+        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321; Football; 9");
         addJamesToFootball.execute(ccaManager, residentManager, eventManager, ui);
         ArrayList<Resident> residents = residentManager.getResidentList();
         assert residents.size() == 2 : "There should be 2 residents.";
@@ -67,23 +67,23 @@ public class ResidentStatsCommandTest {
 
     @Test
     void mostActiveResidents_success() {
-        Command addCcaBasketball = parser.parse("add-cca Basketball HIGH");
+        Command addCcaBasketball = parser.parse("add-cca Basketball; HIGH");
         addCcaBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaFootball = parser.parse("add-cca Football MEDIUM");
+        Command addCcaFootball = parser.parse("add-cca Football; MEDIUM");
         addCcaFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addCcaTennis = parser.parse("add-cca Tennis LOW");
+        Command addCcaTennis = parser.parse("add-cca Tennis; LOW");
         addCcaTennis.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJohn = parser.parse("add-resident John 1234");
+        Command addResidentJohn = parser.parse("add-resident John; 1234");
         addResidentJohn.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJames = parser.parse("add-resident James 4321");
+        Command addResidentJames = parser.parse("add-resident James; 4321");
         addResidentJames.execute(ccaManager, residentManager, eventManager, ui);
-        Command addResidentJane = parser.parse("add-resident Jane 5678");
+        Command addResidentJane = parser.parse("add-resident Jane; 5678");
         addResidentJane.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234 Basketball 9");
+        Command addJohnToBasketball = parser.parse("add-resident-to-cca 1234; Basketball; 9");
         addJohnToBasketball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321 Football 9");
+        Command addJamesToFootball = parser.parse("add-resident-to-cca 4321; Football; 9");
         addJamesToFootball.execute(ccaManager, residentManager, eventManager, ui);
-        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678 Tennis 8");
+        Command addJaneToTennis = parser.parse("add-resident-to-cca 5678; Tennis; 8");
         addJaneToTennis.execute(ccaManager, residentManager, eventManager, ui);
         ArrayList<Resident> residents = residentManager.getResidentList();
         assert residents.size() == 3 :  "There should be 3 residents.";
