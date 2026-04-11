f593f8f3508b006bdd15ce250b0444bae7ed6057
Veer Mehta <e1357046@u.nus.edu>
2026-04-11 18:45:07 +0800

    Add the missing commands in the command list

--- src/main/java/ccamanager/command/HelpCommand.java
+++ src/main/java/ccamanager/command/HelpCommand.java
@@ -9,24 +9,35 @@ public class HelpCommand extends Command {
 
     @Override
     public void execute(CcaManager ccaManager, ResidentManager residentManager, EventManager eventManager, Ui ui) {
-        String help = "Here is a list of all commands:\n" +
-                "> add-cca <cca name>; <level (HIGH, MEDIUM, LOW or UNKNOWN)>\n" +
-                "> view-cca\n" +
-                "> delete-cca <cca name>\n" +
-                "> add-event <event name>; <cca name>; <data time>\n" +
-                "> add-resident <name>; <matric number>\n" +
-                "> delete-resident <matric number>\n" +
-                "> view-resident\n" +
-                "> add-resident-to-cca <matric number>; <cca name>; <points>\n" +
-                "> add-resident-to-event <matric number>; <event name>; <cca name>\n" +
-                "> view-points\n" +
-                "> cca-stats\n" +
-                "> resident-stats\n" +
-                "> view-cca-events <cca name>\n" +
-                "> view-my-events <matric number>\n" +
-                "> help\n" +
-                "> bye";
-        ui.showMessage(help);
+        StringBuilder sb = new StringBuilder();
+        sb.append("--- CCA Manager Help Menu ---\n\n");
+
+        sb.append("[CCA Management]\n");
+        sb.append("> add-cca <name>; <level (HIGH, MEDIUM, LOW, UNKNOWN)>\n");
+        sb.append("> delete-cca <name>\n");
+        sb.append("> view-cca\n");
+        sb.append("> view-exco\n");
+        sb.append("> cca-stats\n\n");
+
+        sb.append("[Resident Management]\n");
+        sb.append("> add-resident <name>; <matric>\n");
+        sb.append("> delete-resident <matric>\n");
+        sb.append("> view-resident\n");
+        sb.append("> add-resident-to-cca <matric>; <cca name>; <points>\n");
+        sb.append("> view-points\n");
+        sb.append("> resident-stats\n\n");
+
+        sb.append("[Event Management]\n");
+        sb.append("> add-event <name>; <cca name>; <date time>\n");
+        sb.append("> add-resident-to-event <matric>; <event name>; <cca name>\n");
+        sb.append("> view-cca-events <cca name>\n");
+        sb.append("> view-my-events <matric>\n\n");
+
+        sb.append("[General]\n");
+        sb.append("> help\n");
+        sb.append("> bye");
+
+        ui.showMessage(sb.toString());
     }
 
     @Override
